cc1e70149376c97532708154f35508895e532f8a
Ramit Roshann <ramitroshan777777@gmail.com>
2026-03-22 16:34:33 +0530

    database connected

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const configureDB = require("./src/config/db");
 const app = express();
 
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+
+configureDB();
 
 //start the server
 app.listen(() => {
