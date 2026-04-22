b14cea3c5978034215baf4d3e5b744492cf4b939
Ramit Roshann <ramitroshan777777@gmail.com>
2026-04-23 03:02:12 +0530

    feat: done with main file of this project

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const configureDB = require("./src/config/db");
-const app = express();
-
 require("dotenv").config();
+
+const app = express();
 const port = process.env.PORT || 5000;
 
 configureDB();
 
+//middlewares
+app.use(express.json());
 
-// app.get("/", async){
-//   console.log("get method")
-// };
+//routes
+const authRoutes = require("./src/routes/auth-routes");
+const bookingRoutes = require("./src/routes/booking-routes");
+
+app.use("/api/auth", authRoutes);
+app.use("/api/bookings", bookingRoutes);
+
+//test routes(just for testing, evrythg working or not)
+app.get("/", (req, res) => {
+  console.log("get method hit");
+  res.send("Just checking API is running");
+});
 
 //start the server
 app.listen(() => {
